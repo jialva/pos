81d427acaacbc77d2b5d8ec94ba71cbddd33ec4e
jino <jino.alva@siincoweb.com>
2021-01-11 10:15:43 -0500

    .gitignore

--- app/config/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	define('RUTA_APP',dirname(dirname(__FILE__)));
-	define('BASE_URL', '/pos/');
-	define('DB_HOST', 'localhost');
-	define('DB_USUARIO', 'root');
-	define('DB_PASSWORD', '');
-	define('DB_NAME', 'pos');
-?>
